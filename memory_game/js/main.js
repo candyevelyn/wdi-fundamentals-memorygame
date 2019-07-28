237a5b5351b4ede2acc23ce3a3c596e854021f50
Candy Miller <candymiller@Candys-MacBook-Pro.local>
2019-07-28 01:04:34 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,6 +24,7 @@ var cards = [
 ];
 var cardsInPlay = [];
 
+
 function checkForMatch()
 {
 	if(cardsInPlay[0] === cardsInPlay[1])
@@ -32,11 +33,11 @@ function checkForMatch()
 	}
 	else
 		alert("Sorry, try again.");
-	
 }
 
-function flipCard(cardId)
+function flipCard()
 {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 
 	cardsInPlay.push(cards[cardId].rank);
@@ -44,9 +45,23 @@ function flipCard(cardId)
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
-	
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2)
+		checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+function createBoard()
+{
+	for(var i = 0; i < cards.length; i++)
+	{
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+
+createBoard();
+
